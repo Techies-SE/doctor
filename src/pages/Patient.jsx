4611948320b78569f1fd466bc7a5ec867ee7e6dd
Eta6531503138 <6531503138@lamduan.mfu.ac.th>
2025-04-08 18:13:33 +0700

    fix partially 6:13pm

--- src/pages/Patient.jsx
+++ src/pages/Patient.jsx
@@ -25,7 +25,7 @@ const Patient = () => {
         <div className="navbar-left">
           <div className="logo-circle"></div>
           <div className="logo-text">
-            <span className="mfu-text">MFU</span>
+            <span className="mfu-text">MFU</span>{" "}
             <span className="wellness-text">Wellness Center</span>
           </div>
         </div>
